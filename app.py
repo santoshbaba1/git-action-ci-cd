10b5bb3764f9af07ac2530b58fe3da05670a501b
Santosh Kumar Sharma <santoshbaba1@rediffmail.com>
2026-04-24 10:38:13 +0530

    Code modify

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Hero-Vired CI/CD"
+    return "Hello Hero-Vired CI/CD Year-2026"
 
 if __name__ == "__main__":
     app.run(debug=True)
