ea4531103f8e57f8d1de524e9921fda632180d4c
Santosh Kumar Sharma <santoshbaba1@rediffmail.com>
2026-04-24 10:49:10 +0530

    Welcome Add

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "Hello Hero-Vired CI/CD Year-2026"
 
+@app.route('/welcome')
+def welcome():
+    return "Hello! Let us learn CI/CD together and become experts in it. Welcome to the Hero-Vired CI/CD Year-2026 course!"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
